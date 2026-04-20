255909ed6eb6faf4229b9ec5e9096b8da6f06603
obigvee <adedejivictor98@gmail.com>
2026-04-20 18:50:17 +0100

    feat: update models.go  data struct

--- models.go
+++ models.go
@@ -7,10 +7,10 @@ type Profile struct {
 	Name string `db:"name" json:"name"`
 	Gender string `db:"gender" json:"gender"`
 	GenderProbability  float64   `db:"gender_probability" json:"gender_probability"`
-	SampleSize         int       `db:"sample_size" json:"sample_size"`
 	Age                int       `db:"age" json:"age"`
 	AgeGroup           string    `db:"age_group" json:"age_group"`
 	CountryID          string    `db:"country_id" json:"country_id"`
+	CountryName        string    `db:"country_name" json:"country_name"`
 	CountryProbability float64   `db:"country_probability" json:"country_probability"`
 	CreatedAt          time.Time `db:"created_at" json:"created_at"`
 
@@ -22,7 +22,6 @@ type GenderizeResp struct {
 	Count       int     `json:"count"`
 
 }
-
 
 type NationalizeResp struct {
 	Country []struct {
@@ -43,4 +42,21 @@ type ProfileListResp struct {
 	Age       int    `json:"age" db:"age"`
 	AgeGroup  string `json:"age_group" db:"age_group"`
 	CountryID string `json:"country_id" db:"country_id"`
+	CountryName string `json:"country_name" db:"country_name"`
+	CountryProbability float64 `json:"country_probability" db:"country_probability"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+
+}
+
+type ProfileResponse struct {
+	Status string `json:"status"`
+	Page int `json:"page"`
+	Limit int `json:"limit"`
+	Total int `json:"total"`
+	Data []Profile `json:"data"`
+}
+
+type ErrorResponse struct {
+	Status string `json:"status"`
+	Message string `json:"message"`
 }
